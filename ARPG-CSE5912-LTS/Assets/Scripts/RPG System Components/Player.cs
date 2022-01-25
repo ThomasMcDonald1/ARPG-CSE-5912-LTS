4ec998df52dd7d19a427b1173d46536ca11ef6d7
ScytheTempo <57293716+ScytheTempo@users.noreply.github.com>
2022-01-24 21:39:08 -0500

    Background Music and Footsteps

--- ARPG-CSE5912-LTS/Assets/Scripts/RPG System Components/Player.cs
+++ ARPG-CSE5912-LTS/Assets/Scripts/RPG System Components/Player.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Player : MonoBehaviour
 {
@@ -9,15 +10,36 @@ public class Player : MonoBehaviour
     public DialogueUI DialogueUI => dialogueUI;
 
     public IInteractable Interactable { get; set; }
+    private Vector3 playerVelocity;
+    private bool isMoving;
+    private bool soundPlaying = false;
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
         //if (dialogueUI.IsOpen) return;
+        playerVelocity = GetComponent<NavMeshAgent>().velocity;
+        if(playerVelocity.magnitude > 0)
+        {
+            isMoving = true;
+        }
+        else
+        {
+            isMoving = false;
+        }
+        if (isMoving && !soundPlaying)
+        {
+            FindObjectOfType<AudioManager>().Play("Footsteps");
+            soundPlaying = true;
+        }
+        else if (!isMoving)
+        {
+            FindObjectOfType<AudioManager>().Stop("Footsteps");
+            soundPlaying = false;
+        }
     }
 }
