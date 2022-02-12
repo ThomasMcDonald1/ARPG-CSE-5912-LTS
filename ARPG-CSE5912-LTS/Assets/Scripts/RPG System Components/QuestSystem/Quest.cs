baea6f236bdbee36c0b28e139e223b616f8da8e3
HankLiu123 <liu.7220@buckeyemail.osu.edu>
2022-02-12 13:56:29 -0500

    update quest system

--- ARPG-CSE5912-LTS/Assets/Scripts/RPG System Components/QuestSystem/Quest.cs
+++ ARPG-CSE5912-LTS/Assets/Scripts/RPG System Components/QuestSystem/Quest.cs
@@ -5,13 +5,22 @@ using UnityEngine;
 [System.Serializable]
 public class Quest 
 {
+    public bool isActive;
     // Start is called before the first frame update
     public string title;
     public string description;
     public int expReward;
     public int goldReward;
 
-    public bool isActive;
+    public QuestGoal goal;
 
-
+    /*
+     * TODO: we will imply the complete in other places 
+     * 
+     */
+    public void Complete()
+    {
+        isActive = false;
+        Debug.Log(title + " is complete!");
+    }
 }
