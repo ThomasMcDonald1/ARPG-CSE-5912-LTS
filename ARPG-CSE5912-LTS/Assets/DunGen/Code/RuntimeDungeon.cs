c209727a8c5b6577a2b5f368298496c4c02aaf0a
Ann Yan <yan.1098@osu.edu>
2022-02-14 22:12:49 -0500

    dungen navmesh generation

--- ARPG-CSE5912-LTS/Assets/DunGen/Code/RuntimeDungeon.cs
+++ ARPG-CSE5912-LTS/Assets/DunGen/Code/RuntimeDungeon.cs
@@ -4,21 +4,26 @@ using System.Linq;
 using System.Text;
 using UnityEngine;
 using DunGen.Adapters;
+using UnityEngine.AI;
 
 namespace DunGen
 {
     [AddComponentMenu("DunGen/Runtime Dungeon")]
+
 	public class RuntimeDungeon : MonoBehaviour
 	{
         public DungeonGenerator Generator = new DungeonGenerator();
         public bool GenerateOnStart = true;
 		public GameObject Root;
 
-
-        protected virtual void Start()
+        protected virtual void Awake()
         {
 			if (GenerateOnStart)
+			{
 				Generate();
+				BakeNavMesh();
+				Debug.Log("Dungeon Generated!");
+			}
 		}
 
 		public void Generate()
@@ -29,5 +34,18 @@ namespace DunGen
 			if (!Generator.IsGenerating)
 				Generator.Generate();
 		}
+
+		void BakeNavMesh()
+        {
+			foreach (Transform child in Root.transform)
+			{
+				if (child.CompareTag("DungeonPart"))
+				{
+					child.GetComponent<NavMeshSurface>().BuildNavMesh();
+				}
+			}
+		}
+
+
 	}
 }
