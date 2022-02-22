80f08f99a05e7d7aa82a3a46c522d7cbe76da9cb
jacksewe <jacksonjiang12@gmail.com>
2022-02-22 12:12:23 -0500

    working test quest icons

--- ARPG-CSE5912-LTS/Assets/Scripts/RPG System Components/QuestSystem/Quest.cs
+++ ARPG-CSE5912-LTS/Assets/Scripts/RPG System Components/QuestSystem/Quest.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 [System.Serializable]
 public class Quest 
 {
-    public bool isActive;
+    //public bool isActive;
     [SerializeField] private string title;
     [SerializeField] private string description;
     [SerializeField] private KillingGoal[] killingGoals;
--- ARPG-CSE5912-LTS/Assets/Scripts/RPG System Components/QuestSystem/QuestGiver.cs
+++ ARPG-CSE5912-LTS/Assets/Scripts/RPG System Components/QuestSystem/QuestGiver.cs
@@ -1,13 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
-using UnityEngine.UI;
-using TMPro;
+
 
 public class QuestGiver : MonoBehaviour
 {
 
-
+    private int questIndex;// used to know what quest to give
     [SerializeField] private Quest[] quests;
     [SerializeField] private Texture2D questionIcon, questionIconGray, exclamationIcon;
     [SerializeField] private Sprite questionIconSprite, questionIconGraySprite, exclamationIconSprite;
@@ -15,16 +13,30 @@ public class QuestGiver : MonoBehaviour
     [SerializeField] private QuestLog testLog;//for testing only
     void Start()
     {
+        questIndex = 0;
         //create sprites from textures
         questionIconSprite = Sprite.Create(questionIcon, new Rect(0.0f, 0.0f, questionIcon.width, questionIcon.height), new Vector2(0.5f, 0.5f), 100.0f);
         questionIconGraySprite = Sprite.Create(questionIconGray, new Rect(0.0f, 0.0f, questionIconGray.width, questionIconGray.height), new Vector2(0.5f, 0.5f), 100.0f);
         exclamationIconSprite = Sprite.Create(exclamationIcon, new Rect(0.0f, 0.0f, exclamationIcon.width, exclamationIcon.height), new Vector2(0.5f, 0.5f), 100.0f);
-
+        iconRenderer.sprite = exclamationIconSprite;
         foreach (Quest quest in quests)
         {
             quest.QuestGiverReference = this;
         }
     }
+    //test
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (!QuestLog.QuestLogInstance.HasQuest(quests[questIndex]))
+            {
+                QuestLog.QuestLogInstance.AddQuest(quests[questIndex]);
+            }
+            UpdateQuestIcon();
+        }
+    }
+
     public Quest[] Quests
     {
         get
@@ -35,24 +47,42 @@ public class QuestGiver : MonoBehaviour
 
     public void UpdateQuestIcon()
     {
-        foreach(Quest quest in quests)
+        if (quests[questIndex].IsComplete && QuestLog.QuestLogInstance.HasQuest(quests[questIndex]))
         {
-            if(quest != null)
+            iconRenderer.sprite = questionIconSprite;
+            questIndex++;
+            if(Quests[questIndex] == null)//if have no more quests
             {
-                if(quest.IsComplete && QuestLog.QuestLogInstance.HasQuest(quest))
-                {
-                    iconRenderer.sprite = questionIconSprite;
-                    break;//turn into question when one quest is complete, might need to change later since we might have just one quest active at a time
-                }
-                else if(!QuestLog.QuestLogInstance.HasQuest(quest)){
-                    iconRenderer.sprite = exclamationIconSprite;
-                }
-                else if(!quest.IsComplete && QuestLog.QuestLogInstance.HasQuest(quest))
-                {
-                    iconRenderer.sprite = questionIconGraySprite;
-                }
+                iconRenderer.sprite = null;
             }
         }
+        else if (!QuestLog.QuestLogInstance.HasQuest(quests[questIndex]))
+        {
+            iconRenderer.sprite = exclamationIconSprite;
+
+        }
+        else if (!quests[questIndex].IsComplete && QuestLog.QuestLogInstance.HasQuest(quests[questIndex]))
+        {
+            iconRenderer.sprite = questionIconGraySprite;
+        }
+        //foreach(Quest quest in quests)
+        //{
+        //    if(quest != null)
+        //    {
+        //        if(quest.IsComplete && QuestLog.QuestLogInstance.HasQuest(quest))
+        //        {
+        //            iconRenderer.sprite = questionIconSprite;
+        //            break;//turn into question when one quest is complete, might need to change later since we might have just one quest active at a time
+        //        }
+        //        else if(!QuestLog.QuestLogInstance.HasQuest(quest)){
+        //            iconRenderer.sprite = exclamationIconSprite;
+        //        }
+        //        else if(!quest.IsComplete && QuestLog.QuestLogInstance.HasQuest(quest))
+        //        {
+        //            iconRenderer.sprite = questionIconGraySprite;
+        //        }
+        //    }
+        //}
     }
 
     public void TestAddQuest()
@@ -66,7 +96,7 @@ public class QuestGiver : MonoBehaviour
     public void TestKill()
     {
         //testing
-       // testLog.selectedQuest.KillingGoals[0].CurrentAmount++;
+        // testLog.selectedQuest.KillingGoals[0].CurrentAmount++;
     }
 
 
