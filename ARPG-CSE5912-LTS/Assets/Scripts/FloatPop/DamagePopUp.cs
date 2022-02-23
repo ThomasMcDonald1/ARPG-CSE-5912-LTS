7578e6f1b15b80f532023d825dcbdab8b52a2ec4
pupu98 <77910363+pupu98@users.noreply.github.com>
2022-02-23 00:24:50 -0500

    basic things for pop

--- ARPG-CSE5912-LTS/Assets/Scripts/FloatPop/DamagePopUp.cs
+++ ARPG-CSE5912-LTS/Assets/Scripts/FloatPop/DamagePopUp.cs
@@ -6,14 +6,36 @@ using TMPro;
 public class DamagePopUp : MonoBehaviour
 {
     private TextMeshProUGUI popUpText;
+    private float disappearTimer;
+    private float disappearSpeed;
+    private Color textColor;
 
     private void Awake()
     {
         popUpText = transform.GetComponent<TextMeshProUGUI>();
+        
     }
 
     public void Setup(int damageAmount)
     {
         popUpText.text = damageAmount.ToString();
+        textColor = popUpText.color;
+        disappearTimer = 1f;
+        disappearSpeed = 2f;
+    }
+
+    private void Update()
+    {
+        disappearTimer -= Time.deltaTime;
+        if (disappearTimer < 0)
+        {
+            textColor.a -= disappearSpeed * Time.deltaTime;
+            popUpText.color = textColor;
+
+            if (textColor.a <= 0)
+            {
+                Destroy(gameObject);
+            }
+        }
     }
 }
--- ARPG-CSE5912-LTS/Assets/Scripts/FloatPop/PopTesting.cs
+++ ARPG-CSE5912-LTS/Assets/Scripts/FloatPop/PopTesting.cs
@@ -4,13 +4,33 @@ using UnityEngine;
 
 public class PopTesting : MonoBehaviour
 {
+    
     [SerializeField] private Transform DamagePop;
-    private void Start()
+
+    public void CreateDamagePop(Character target, int damageAmount, bool isCrit)
     {
+        DamagePopUp damagePopUp;
         Transform damagePopUpTransform = Instantiate(DamagePop, Vector3.zero, Quaternion.identity);
-        damagePopUpTransform.SetParent(transform, false);
-        DamagePopUp damagePopUp = damagePopUpTransform.GetComponent<DamagePopUp>();
-        damagePopUp.Setup(300);
+        damagePopUpTransform.SetParent(target.transform.Find("EnemyCanvas"), false);
+        damagePopUpTransform.position -= new Vector3(0f, 3f, 0f);
+        damagePopUp = damagePopUpTransform.GetComponent<DamagePopUp>();
+        damagePopUp.Setup(damageAmount);
+
+    }
+    private void OnEnable()
+    {
+        DamageAbilityEffect.AbilityDamageReceivedEvent += OnDamagePop;
+    }
+
+    private void OnDisable()
+    {
+        DamageAbilityEffect.AbilityDamageReceivedEvent -= OnDamagePop;
+    }
+
+    public void OnDamagePop(object sender, InfoEventArgs<(Character, int, bool)> e)
+    {
+        (Character damageTarget, int damageAmount, bool isCrit) = e.info;
+        CreateDamagePop(damageTarget, damageAmount, isCrit);
     }
 
 }
