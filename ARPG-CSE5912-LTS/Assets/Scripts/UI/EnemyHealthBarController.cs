59b06f61e35d6e11a798504fa8d990f38cc861a1
jacksewe <jacksonjiang12@gmail.com>
2022-02-27 15:22:27 -0500

    lerping enemy healthbars

--- ARPG-CSE5912-LTS/Assets/Scripts/UI/EnemyHealthBarController.cs
+++ ARPG-CSE5912-LTS/Assets/Scripts/UI/EnemyHealthBarController.cs
@@ -3,74 +3,91 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class EnemyHealthBarController : MonoBehaviour
+public class EnemyHealthBarController : HealthBarController
 {
-    public Image healthBar;
-    //public float maxHealth = 100f;
-    //public float currHealth;
-
-    Stats stats;
-    float lerpSpd;
-
-    // PlayerController_Script Player;  for updating current player health, not using at this point
+    public Slider lerpBar;
 
 
 
-    private void Start()
+    protected override void Start()
     {
-        // currHealth = maxHealth;
-
-        //stats = GameObject.FindGameObjectWithTag("Enemy").GetComponent<Stats>();
-        stats = gameObject.GetComponent<Stats>();
-        //maxHealth = stats.maxHealth;
-
-        Debug.Log(stats[StatTypes.MaxHP]);
-        //stats.health = stats.maxHealth;
-        //currHealth = stats.health;
+        base.Start();
+        lerpBar.maxValue = healthBar.maxValue;
+        lerpBar.value = healthBar.value;
     }
-
-    private void Update()
+    protected override void UpdateSlider()
     {
-        if (stats[StatTypes.HP] > stats[StatTypes.MaxHP]) stats[StatTypes.HP] = stats[StatTypes.MaxHP];
-        lerpSpd = 10f * Time.deltaTime;
-        HealthBarFiller();
-        colorChanger();
-        //currHealth = stats.health;
+        float lerpSpd = 10f;
+        base.UpdateSlider();
+        lerpBar.value = Mathf.Lerp(lerpBar.value, healthBar.value, Time.deltaTime *lerpSpd);
 
     }
 
-    /* change the health bar with a lerp speed*/
-    public void HealthBarFiller()
-    {
-        //Debug.Log("Health" + stats[StatTypes.HEALTH]);
-        Debug.Log("Max Health" + stats[StatTypes.MaxHP]);
-        //need float division
-        healthBar.fillAmount = Mathf.Lerp(healthBar.fillAmount, (float)stats[StatTypes.HP] / (float)stats[StatTypes.MaxHP], lerpSpd);
-    }
-    public void colorChanger()
-    {
-        //need fload division
-        Color healthC = Color.Lerp(Color.red, Color.green, (float)stats[StatTypes.HP] / (float)stats[StatTypes.MaxHP]);
-        healthBar.color = healthC;
-
-    }
-    //public void HitDamage(float damageRate)
-    //{
-    //    if(stats[StatTypes.HEALTH] > 0)
-    //    {
-    //        stats[StatTypes.HEALTH] -= damageRate;
-    //    }
-    //}
-    //public void healing(float healingRate)
-    //{
-    //    if (stats[StatTypes.HEALTH] < 100)
-    //    {
-
-    //        stats[StatTypes.HEALTH] += healingRate;
-    //        if (stats[StatTypes.HEALTH] > 100)
-    //        {
-    //            stats[StatTypes.HEALTH] = 100;
-    //        }
-    //    }
-    //}
 }
+//    //public Image healthBar;
+
+//    //public float maxHealth = 100f;
+//    //public float currHealth;
+
+//    float lerpSpd;
+
+//    // PlayerController_Script Player;  for updating current player health, not using at this point
+
+
+
+//    private void Start()
+//    {
+//        // currHealth = maxHealth;
+
+//        //stats = GameObject.FindGameObjectWithTag("Enemy").GetComponent<Stats>();
+//        //maxHealth = stats.maxHealth;
+
+//        //stats.health = stats.maxHealth;
+//        //currHealth = stats.health;
+//    }
+
+//    private void Update()
+//    {
+//        if (stats[StatTypes.HP] > stats[StatTypes.MaxHP]) stats[StatTypes.HP] = stats[StatTypes.MaxHP];
+//        lerpSpd = 10f * Time.deltaTime;
+//        HealthBarFiller();
+//        colorChanger();
+//        //currHealth = stats.health;
+
+//    }
+
+//    /* change the health bar with a lerp speed*/
+//    public void HealthBarFiller()
+//    {
+//        //Debug.Log("Health" + stats[StatTypes.HEALTH]);
+//        Debug.Log("Max Health" + stats[StatTypes.MaxHP]);
+//        //need float division
+//        healthBar.fillAmount = Mathf.Lerp(healthBar.fillAmount, (float)stats[StatTypes.HP] / (float)stats[StatTypes.MaxHP], lerpSpd);
+//    }
+//    public void colorChanger()
+//    {
+//        //need fload division
+//        Color healthC = Color.Lerp(Color.red, Color.green, (float)stats[StatTypes.HP] / (float)stats[StatTypes.MaxHP]);
+//        healthBar.color = healthC;
+
+//    }
+//    //public void HitDamage(float damageRate)
+//    //{
+//    //    if(stats[StatTypes.HEALTH] > 0)
+//    //    {
+//    //        stats[StatTypes.HEALTH] -= damageRate;
+//    //    }
+//    //}
+//    //public void healing(float healingRate)
+//    //{
+//    //    if (stats[StatTypes.HEALTH] < 100)
+//    //    {
+
+//    //        stats[StatTypes.HEALTH] += healingRate;
+//    //        if (stats[StatTypes.HEALTH] > 100)
+//    //        {
+//    //            stats[StatTypes.HEALTH] = 100;
+//    //        }
+//    //    }
+//    //}
+//}
