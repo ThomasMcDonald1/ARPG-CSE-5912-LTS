53d8f25c9f36e3dbadc83d97072f6e03e6a5df76
HankLiu123 <liu.7220@buckeyemail.osu.edu>
2022-03-22 18:34:21 -0400

    tooltips fixed

--- ARPG-CSE5912-LTS/Assets/Scripts/UI/InventorySlot.cs
+++ ARPG-CSE5912-LTS/Assets/Scripts/UI/InventorySlot.cs
@@ -85,12 +85,14 @@ public class InventorySlot : MonoBehaviour, IPointerEnterHandler, IPointerExitHa
         tipText.text = tip;
         tipWindow.sizeDelta = new Vector2(tipText.preferredWidth > 200 ? 200 : tipText.preferredWidth, tipText.preferredHeight);
         tipWindow.gameObject.SetActive(true);
-        tipWindow.transform.position = new Vector2(mousePos.x, mousePos.y);
+        tipWindow.transform.position = new Vector2(mousePos.x+ tipWindow.sizeDelta.x/2, mousePos.y);
 
 
     }
     public void HideTip()
     {
+        Debug.Log("Hide ShowMessage");
+
         tipText.text = default;
         tipWindow.gameObject.SetActive(false);
     }
