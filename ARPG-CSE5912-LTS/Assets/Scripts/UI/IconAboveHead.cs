7812b6b3d91a67290bea22c833c30258313deb6c
jacksewe <jacksonjiang12@gmail.com>
2022-03-23 21:02:54 -0400

    changed enemyhealthbar to iconabovehead
    since it has multiple uses

--- ARPG-CSE5912-LTS/Assets/Scripts/UI/IconAboveHead.cs
+++ ARPG-CSE5912-LTS/Assets/Scripts/UI/IconAboveHead.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyHealthBar : MonoBehaviour
+public class IconAboveHead : MonoBehaviour
 {
     // Start is called before the first frame update
     private void Start()
