8ae780de05310af90bd6d8497b23799c98f01d53
pupu98 <77910363+pupu98@users.noreply.github.com>
2022-04-26 00:34:01 -0400

    add range for all enemy avoid error

--- ARPG-CSE5912-LTS/Assets/Scripts/RPG System Components/Characters/Enemy.cs
+++ ARPG-CSE5912-LTS/Assets/Scripts/RPG System Components/Characters/Enemy.cs
@@ -81,6 +81,7 @@ namespace ARPG.Combat
                     EnemyAttackTypeList.Add(enemyability);
                 }
             }
+            AbilityRange = 50;
             cooldownTimer = 6;
             timeChecker = cooldownTimer;
         }
